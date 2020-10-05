3f76517a98b0003daae20f92c8860c8a52b2062f
GastonDonnet <dgastonalain@gmail.com>
2020-10-05 19:03:04 -0300

    Arregla bug de selector selectCartTotal

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -18,5 +18,8 @@ export const selectCartItemsCount = createSelector(
 );
 
 export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
-  cartItems.reduce((acc, cartItem) => acc + cartItem.price, 0)
+  cartItems.reduce(
+    (acc, cartItem) => acc + cartItem.price * cartItem.quantity,
+    0
+  )
 );
