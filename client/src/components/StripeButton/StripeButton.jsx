625b9b050516957660476cdb622a74dd75105553
GastonDonnet <dgastonalain@gmail.com>
2020-10-16 16:42:43 -0300

    Termina de crear backend para pago con stripe

--- client/src/components/StripeButton/StripeButton.jsx
+++ client/src/components/StripeButton/StripeButton.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios'
 import StripeCheckout from "react-stripe-checkout";
 
 export const StripeCheckoutButton = ({ price }) => {
@@ -7,8 +8,20 @@ export const StripeCheckoutButton = ({ price }) => {
     "pk_test_51HZLJnCpwfvtfGvhP2INZVlN3UVVwki7Ck9m1lcbVcbekqU8jW9FNGbeEsvaPx5VCWLM4fciF7W4iChB9Mz8SaZu00nhctcVwJ";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Se realizo el pago exitosamente!");
+    axios({
+      url: 'http://localhost:5000/crwn-clothing/payment',
+      method: 'post',
+      data: {
+        token,
+        amount: priceForStripe
+      }
+    }).then(res=>{
+      alert("Se realizo el pago exitosamente!");
+    }).catch(err=>{
+      console.log("Error en el pago", (err))
+      alert("Ocurrio un error con tu pago!")
+    })
+    
   };
 
   return (
